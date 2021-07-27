f0de683ac4eb3c10dce1db45dc1f4e1e43da7b6c
Henrique Rodrigues <henrique.rodrigues@wildlifestudios.com>
2021-07-26 22:12:03 -0300

    temporarily put Nick Cage's photo as profile

--- client/lemon_pie/src/components/SummaryBox.js
+++ client/lemon_pie/src/components/SummaryBox.js
@@ -1,5 +1,6 @@
 import { Grid, Paper, Typography, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import nick from "../assets/nick.jpeg";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -16,6 +17,14 @@ const useStyles = makeStyles((theme) => ({
   gridItem: {
     fontSize: "63px",
   },
+  photo: {
+    height: "100px",
+    width: "100px",
+    marginLeft: theme.spacing(12),
+  },
+  profileName: {
+    marginRight: theme.spacing(20),
+  },
 }));
 
 const SummaryBox = ({ user, votes }) => {
@@ -23,7 +32,14 @@ const SummaryBox = ({ user, votes }) => {
 
   return (
     <Paper key={user.key} className={classes.paper}>
-      <Typography variant="h4">{user.name}</Typography>
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <img src={nick} alt="profile" className={classes.photo} />
+        <Box width="100%">
+          <Typography variant="h4" className={classes.profileName}>
+            {user.name}
+          </Typography>
+        </Box>
+      </Box>
       <Grid container key={user.key} className={classes.gridContainer}>
         {Object.keys(votes).map((emojiKey) => (
           <Grid key={emojiKey} item lg={3} md={4} sm={6} xs={12}>
